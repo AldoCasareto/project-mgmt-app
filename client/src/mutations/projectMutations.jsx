afaa5d824ee1cdf9cba691291345c2f2eedaec20
Aldo <acasareto@gmail.com>
2022-11-23 11:31:38 +0100

    editProject and deleteProject mutation done

--- client/src/mutations/projectMutations.jsx
+++ client/src/mutations/projectMutations.jsx
@@ -21,3 +21,30 @@ export const CREATE_PROJECT = gql`
     }
   }
 `;
+
+export const DELETE_PROJECT = gql`
+  mutation deleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      name
+      id
+    }
+  }
+`;
+
+export const UPDATE_PROJECT = gql`
+  mutation updateProject($id: ID!, $name: String!, $description: String!, $status: String!) {
+    updateProject(name: $name, description: $description, status: $status, client: $client)
+  }
+  {
+    id
+    name
+    description
+    status
+    client {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
